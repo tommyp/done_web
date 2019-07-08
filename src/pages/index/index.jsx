91dc9968d2e12158f4613b380855b144c7f36c9b
Tommy Palmer <hi@tommyp.org>
2019-07-08 23:10:52 +0100

    connect apollo to backend

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const LIST_ITEMS = gql`
+  {
+    items {
+      name
+    }
+  }
+`
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <h1>Hello</h1>
+      <Query query={LIST_ITEMS}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return "Error";
+
+          return (
+            <ul>
+              {data.items.map(item => (
+                <li>{item.name}</li>
+              ))}
+            </ul>
+          )
+        }}
+      </Query>
     )
   }
 }
